19dba988a9d42efb35eab8ecde5cd8cef180fbc3
Gabriel Reis <gabriieelreeis@gmail.com>
2023-01-11 14:29:40 -0300

    Update introduction_slider.dart

--- lib/source/presentation/pages/introduction_slider.dart
+++ lib/source/presentation/pages/introduction_slider.dart
@@ -8,7 +8,7 @@ import '../widgets/widgets.dart';
 class IntroductionSlider extends StatefulWidget {
   /// Defines the appearance of the introduction slider items that are arrayed
   /// within the introduction slider.
-  final List<IntroductionSliderItem> items;
+  final List<dynamic> items;
 
   /// Determines the physics of a [Scrollable] widget.
   final ScrollPhysics? physics;
@@ -33,6 +33,10 @@ class IntroductionSlider extends StatefulWidget {
   /// Show and hide app status/navigation bar on the introduction slider.
   final bool showStatusBar;
 
+  final Widget? customFullNextButton;
+
+  final Widget? customLastItemButton;
+
   /// The initial page index of the introduction slider.
   int initialPage;
 
@@ -48,6 +52,8 @@ class IntroductionSlider extends StatefulWidget {
     this.next,
     this.showDots = true,
     this.dotIndicator,
+    this.customFullNextButton,
+    this.customLastItemButton,
   })  : assert((initialPage <= items.length - 1) && (initialPage >= 0),
             "initialPage can't be less than 0 or greater than items length."),
         super(key: key);
@@ -107,26 +113,30 @@ class _IntroductionSliderState extends State<IntroductionSlider> {
             onPageChanged: (index) =>
                 setState(() => widget.initialPage = index),
             itemBuilder: (context, index) {
-              return Container(
-                decoration: BoxDecoration(
-                  color: widget.items[index].backgroundColor,
-                  gradient: widget.items[index].gradient,
-                  image: widget.items[index].backgroundImageDecoration,
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Spacer(),
-                    widget.items[index].logo ?? const SizedBox(),
-                    widget.items[index].title ?? const SizedBox(),
-                    widget.items[index].subtitle ?? const SizedBox(),
-                    const Spacer(),
-                  ],
-                ),
-              );
+              if (widget.items[index].runtimeType == IntroductionSliderItem) {
+                return Container(
+                  decoration: BoxDecoration(
+                    color: widget.items[index].backgroundColor,
+                    gradient: widget.items[index].gradient,
+                    image: widget.items[index].backgroundImageDecoration,
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Spacer(),
+                      widget.items[index].logo ?? const SizedBox(),
+                      widget.items[index].title ?? const SizedBox(),
+                      widget.items[index].subtitle ?? const SizedBox(),
+                      const Spacer(),
+                    ],
+                  ),
+                );
+              } else {
+                return widget.items[index];
+              }
             },
           ),
-          widget.dotIndicator == null
+          widget.dotIndicator == null || !widget.showDots
               ? const SizedBox()
               : Positioned(
                   bottom: 80,
@@ -157,71 +167,92 @@ class _IntroductionSliderState extends State<IntroductionSlider> {
                     ),
                   ),
                 ),
-          Positioned(
-            bottom: 35,
-            child: SizedBox(
-              width: MediaQuery.of(context).size.width,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  (widget.initialPage == 0 || widget.back == null)
-                      ? const SizedBox()
-                      : TextButton(
-                          onPressed: () => pageController.previousPage(
-                            duration: widget.back!.animationDuration!,
-                            curve: widget.back!.curve!,
-                          ),
-                          style: widget.back!.style,
-                          child: widget.back!.child,
-                        ),
-                  lastIndex
-                      ? TextButton(
-                          onPressed: () {
-                            Navigator.of(context).pushReplacement(
-                              PageRouteBuilder(
-                                transitionDuration:
-                                    widget.done.animationDuration!,
-                                transitionsBuilder: (context, animation,
-                                    secondAnimation, child) {
-                                  animation = CurvedAnimation(
-                                    parent: animation,
-                                    curve: widget.done.curve!,
-                                  );
-                                  return SlideTransition(
-                                    position: Tween<Offset>(
-                                      begin: widget.scrollDirection ==
-                                              Axis.vertical
-                                          ? const Offset(0, 1.0)
-                                          : const Offset(1.0, 0.0),
-                                      end: Offset.zero,
-                                    ).animate(animation),
-                                    child: child,
-                                  );
-                                },
-                                pageBuilder:
-                                    (context, animation, secondaryAnimation) {
-                                  return widget.done.home!;
-                                },
-                              ),
-                            );
-                          },
-                          style: widget.done.style,
-                          child: widget.done.child,
-                        )
-                      : widget.next == null
-                          ? const SizedBox()
-                          : TextButton(
-                              onPressed: () => pageController.nextPage(
-                                duration: widget.next!.animationDuration!,
-                                curve: widget.next!.curve!,
-                              ),
-                              style: widget.next!.style,
-                              child: widget.next!.child,
+          if (widget.customFullNextButton == null)
+            Positioned(
+              bottom: 35,
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    (widget.initialPage == 0 || widget.back == null)
+                        ? const SizedBox()
+                        : TextButton(
+                            onPressed: () => pageController.previousPage(
+                              duration: widget.back!.animationDuration!,
+                              curve: widget.back!.curve!,
                             ),
-                ],
+                            style: widget.back!.style,
+                            child: widget.back!.child,
+                          ),
+                    lastIndex
+                        ? TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pushReplacement(
+                                PageRouteBuilder(
+                                  transitionDuration:
+                                      widget.done.animationDuration!,
+                                  transitionsBuilder: (context, animation,
+                                      secondAnimation, child) {
+                                    animation = CurvedAnimation(
+                                      parent: animation,
+                                      curve: widget.done.curve!,
+                                    );
+                                    return SlideTransition(
+                                      position: Tween<Offset>(
+                                        begin: widget.scrollDirection ==
+                                                Axis.vertical
+                                            ? const Offset(0, 1.0)
+                                            : const Offset(1.0, 0.0),
+                                        end: Offset.zero,
+                                      ).animate(animation),
+                                      child: child,
+                                    );
+                                  },
+                                  pageBuilder:
+                                      (context, animation, secondaryAnimation) {
+                                    return widget.done.home!;
+                                  },
+                                ),
+                              );
+                            },
+                            style: widget.done.style,
+                            child: widget.done.child,
+                          )
+                        : widget.next == null
+                            ? const SizedBox()
+                            : TextButton(
+                                onPressed: () => pageController.nextPage(
+                                  duration: widget.next!.animationDuration!,
+                                  curve: widget.next!.curve!,
+                                ),
+                                style: widget.next!.style,
+                                child: widget.next!.child,
+                              ),
+                  ],
+                ),
               ),
             ),
-          ),
+          if (widget.customFullNextButton != null)
+            Positioned(
+              bottom: 35,
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width,
+                child: GestureDetector(
+                  onTap: () {
+                    if (!lastIndex) {
+                      pageController.nextPage(
+                        duration: const Duration(milliseconds: 500),
+                        curve: Curves.easeInOut,
+                      );
+                    }
+                  },
+                  child: !lastIndex
+                      ? widget.customFullNextButton
+                      : widget.customLastItemButton,
+                ),
+              ),
+            ),
         ],
       ),
     );
